6e108ffeb8a3037ae903b5451d57a7d4dd959917
spxtwbi2022-ship-it <spxtwbi2022@gmail.com>
2026-01-07 14:26:52 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+
+// 設定檔
+const GAS_ENDPOINT = 'https://script.google.com/macros/s/AKfycbw9HIEg9Q4tquFEc7JRIwmsg_aweIQJ-Adbn0aCrGLUQun8Xy0G2e72WjFrMx9rulGo/exec';
+const API_KEY = ''; // 若你設定後端驗證，可放 key
+const BATCH_SIZE = 20; // 到達多少筆就送出
+const BATCH_TIMEOUT_MS = 15000; // 最久幾毫秒會自動送出
